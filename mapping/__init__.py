abf71a128838c72086ca775b1c1d4cee5bebf9fb
hamishgibbs <50525951+hamishgibbs@users.noreply.github.com>
2019-10-16 15:06:33 +0100

    Add files via upload

---
+++ mapping/__init__.py
@@ -0,0 +1,8 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from shapely.geometry import mapping, shape
+import numpy as np
+from math import radians, degrees, sin, cos, asin, acos, sqrt
+
+name = 'Galapagos_Mapper'
+
