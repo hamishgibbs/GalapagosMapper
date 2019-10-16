6b238e1ea6397d788ffe5cc67073e6afc1fd9e4e
hamishgibbs <50525951+hamishgibbs@users.noreply.github.com>
2019-10-16 15:36:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 import setuptools
 
-with open('README.md', 'r') as fh:
-      long_description = fh.read()
-
 setuptools.setup(
     name='mapping',
     version ='0.0.1',
     author='Hamish Gibbs',
     description='Package for plotting data in the Galapagos Islands',
-    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/hamishgibbs/Galapagos_Mapper',
     packages=['mapping'],
