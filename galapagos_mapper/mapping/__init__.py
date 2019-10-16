189bbac389eee1e1dcbf3af04d567eb1bbfb965d
hamishgibbs <50525951+hamishgibbs@users.noreply.github.com>
2019-10-16 15:06:05 +0100

    Delete __init__.py

--- galapagos_mapper/mapping/__init__.py
+++
@@ -1,8 +0,0 @@
-import geopandas as gpd
-import matplotlib.pyplot as plt
-from shapely.geometry import mapping, shape
-import numpy as np
-from math import radians, degrees, sin, cos, asin, acos, sqrt
-
-name = 'Galapagos_Mapper'
-
