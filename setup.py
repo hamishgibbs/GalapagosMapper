1ee1d3754466ca395ac6d6f8a41d7a7ff062b1d0
hamishgibbs <50525951+hamishgibbs@users.noreply.github.com>
2019-10-16 15:41:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ setuptools.setup(
     description='Package for plotting data in the Galapagos Islands',
     long_description_content_type='text/markdown',
     url='https://github.com/hamishgibbs/Galapagos_Mapper',
-    packages=['mapping'],
+    packages=setuptools.find_packages(),
+    #packages=['mapping'],
+    #package_dir={'': 'C:/Users/bcollins/UHD_PY/uhd/host/build/python'},
     python_requires='>=3.6',
     zip_safe=False
     )
