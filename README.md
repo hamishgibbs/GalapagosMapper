d7b91064444add0f731ee78e7b07e28329235177
hamishgibbs <50525951+hamishgibbs@users.noreply.github.com>
2019-10-17 15:07:15 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Galapagos_Mapper
+Package for plotting data in the Galapagos Islands
+
+![Header Image](https://raw.githubusercontent.com/hamishgibbs/Galapagos_Mapper/master/South_Scale_Test.png)
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='GalapagosMapper',
+    version ='0.0.1',
+    author='Hamish Gibbs',
+    description='Package for matplotlib compatible plotting in the Galapagos Islands',
+    url='https://github.com/hamishgibbs/Galapagos_Mapper',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.6',
+    zip_safe=False
+    )
