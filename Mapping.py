c5a0994fdf162e5754a3f5b234a9075279fe9a44
hamishgibbs <hamishgibbs@Hamishs-MacBook-Air.local>
2019-10-24 17:47:36 +0100

    Mapping.py

--- Mapping.py
+++ Mapping.py
@@ -16,14 +16,19 @@ class GalapagosMap:
 
   import os
   global os
+
+  from shapely.geometry import mapping
+  global mapping
+
+  import csv
+  global csv
   
   from math import radians, degrees, sin, cos, asin, acos, sqrt
   global radians, degrees, sin, cos, asin, acos, sqrt
   
-  #Create bounding boxes when ititiating the class
-#  def __init__(self, volcano_dataset, island_to_plot='Archipelago', fig_size='large'):
-  def __init__(self, island_to_plot='Archipelago', fig_size='large'):
+  def __init__(self):
 
+   #Create bounding boxes when ititiating the class
     bbox = {}
 
     module_directory = os.path.dirname(os.path.abspath(__file__))
@@ -36,13 +41,7 @@ class GalapagosMap:
         bbox[row[0]] = np.array([float(row[1]), float(row[2]), float(row[3]), float(row[4])]) 
 
     self.bounding_boxes = bbox
-    
-    #self.volcanoes = volcano_dataset
-    self.island_to_plot = island_to_plot
-    #self.bounding_boxes = Bounding_Boxes(self.volcanoes)
-    #self.volcano_names = self.bounding_boxes.volcano_names
-    self.volcano_names = bbox.keys()
-    self.fig_size = fig_size
+    self.volcano_names = list(bbox.keys())
 
   #Add x_lim_island and y_lim_island here (remove Bounding_Box Class)
   #Define a method to create matplotlib compatible xlim
@@ -74,29 +73,36 @@ class GalapagosMap:
     return(ymin - zoom, ymax + zoom)
   
   #Plot a basemap with the correct axes, north arrow and scale bar
-  def base_map(self):
+  def base_map(self, island_to_plot='Archipelago', arrow_length_fraction=20, arrow_text_fraction=15, arrow_buffer_fraction=40, 
+               scale_buffer_fraction=10, manual_scale_x=None, manual_scale_y=None):
     
-    #NORTH ARROW CALCULATIONS
-    arrow_buffer = 0.05
-    
-    #Change these values empirically when the final figure sizes are decided
-    #The represent the fraction of the image at which to place the base of the N arrow
-    #and the 'N' text, could also apply to arrow_buffer
-    arrow_length_fraction = {'large' : 25,
-                             'medium' : 15,
-                             'small' : 10,
-                            }
-    
-    arrow_text_fraction = {'large' : 20,
-                           'medium' : 13,
-                           'small' : 8,
-                          }
+    #CALCULATE BUFFERS
+    self.island_to_plot = island_to_plot
     
     x_diff = self.x_lim_island()[1] - self.x_lim_island()[0]
-    arrow_x = self.x_lim_island()[1] - (x_diff * arrow_buffer)
     y_diff = self.y_lim_island()[1] - self.y_lim_island()[0]
-    arrow_y = self.y_lim_island()[1] - (y_diff * arrow_buffer)
+    diff_ratio = y_diff/x_diff
+
+    self.arrow_x_buffer = x_diff / arrow_buffer_fraction
+    self.arrow_y_buffer = self.arrow_x_buffer * diff_ratio
     
+    #Remember: manual_scale_x MUST be used with manual_scale_y
+    if manual_scale_x != None:
+      scale_x_buffer = manual_scale_x
+    else:
+      scale_x_buffer = x_diff / scale_buffer_fraction
+    
+    if manual_scale_y != None:
+      scale_y_buffer = manual_scale_y
+    else:
+      scale_y_buffer = scale_x_buffer * diff_ratio
+
+    
+    #NORTH ARROW CALCULATIONS
+    x_diff = self.x_lim_island()[1] - self.x_lim_island()[0]
+    arrow_x = self.x_lim_island()[1] - self.arrow_x_buffer
+    y_diff = self.y_lim_island()[1] - self.y_lim_island()[0]
+    arrow_y = self.y_lim_island()[1] - self.arrow_y_buffer    
     
     #SCALE BAR CACULATIONS
     def great_circle(lon1, lat1, lon2, lat2):
@@ -107,31 +113,14 @@ class GalapagosMap:
     def dirty_inverse(kilometer_value):
       return((1.0/111.0)*(float(kilometer_value)))
     
-    def scale_bar_coordinates(min_x, min_y, max_x, max_y):
+    def scale_bar_coordinates():
       
-      scale_x_buffer = {'large' : 0.125,
-                        'medium' : 0.1,
-                        'small' : 0.08,
-                       }
-      scale_y_buffer = {'large' : 0.1,
-                        'medium' : 0.05,
-                        'small' : 0.025,
-                       }
-
       #List of possible labels for the scale bar
       scale_bar_labels = [0.25, 0.5, 0.75, 1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 40, 50, 75, 100]
 
-      #Determine the width and height of the plot in degrees
-      width_degrees = max_x - min_x
-      height_degrees = max_y - min_y
-
-      #Determine the x and y buffer for the scale bar
-      scale_x_buffer = width_degrees * scale_x_buffer[self.fig_size]
-      scale_y_buffer = height_degrees * scale_y_buffer[self.fig_size]
-
       #Determine the location of the bottom corners (L & R) of the map
-      left_map_point = [min_x, min_y]
-      right_map_point = [max_x, min_y]
+      left_map_point = [self.x_lim_island()[0], self.y_lim_island()[0]]
+      right_map_point = [self.x_lim_island()[1], self.y_lim_island()[0]]
 
       #Determine the width of the map in kilometers
       map_width_km = great_circle(left_map_point[0], left_map_point[1],
@@ -156,7 +145,7 @@ class GalapagosMap:
       scale_L_coord = scale_R_coord - degree_shift
 
       #Determine the y coordinate for both the R and L corners of the scale bar
-      scale_y_coord = min_y + scale_y_buffer
+      scale_y_coord = self.y_lim_island()[0] + scale_y_buffer
 
       '''
       Add white lines to scale bar at 0.1 to 0.4, and 0.6 to 0.8
@@ -176,15 +165,6 @@ class GalapagosMap:
       #Define the height of the text (half of the distance between the scale bar and the plot frame)
       text_y_coordinate = scale_y_coord - (scale_y_buffer/2)
 
-      #Define a middle text label 1/3 of the way from 0 to closest_scale_bar_label
-      middle_text_label = round(np.linspace(0, closest_scale_bar_label, 6)[2], 1)
-
-      #If the middle_text_label has no decimal place, convert it to an integer
-      if middle_text_label.is_integer():
-        middle_text_label = int(middle_text_label)
-
-      #list of items needed for plotting = scale_L_coord, scale_R_coord, scale_y_coord, white1_x_coords, white2_x_coords, text_y_coordinate, sextiles[2],
-      #middle_text_label, closest_scale_bar_label
       return(
           {'scale_L_coord' : scale_L_coord,
            'scale_R_coord' : scale_R_coord,
@@ -192,35 +172,32 @@ class GalapagosMap:
            'white1_x_coords' : white1_x_coords,
            'white2_x_coords' : white2_x_coords,
            'text_y_coordinate' : text_y_coordinate,
-           'middle_text_x_coord' : sextiles[2],
-           'middle_text_label' : middle_text_label,
            'closest_scale_bar_label' : closest_scale_bar_label
           }
       )
-
     
+    #x_diff = self.x_lim_island()[1] - self.x_lim_island()[0]
+        
+    #item_buffer = x_diff * 0.1
+         
     #Plot the base figure
     self.fig, self.ax = plt.subplots()
     
     #Plot North Arrow
     self.ax.annotate('', xy=(arrow_x, arrow_y), 
-                     xytext=(arrow_x, (arrow_y-(y_diff/arrow_length_fraction[self.fig_size]))), 
+                     xytext=(arrow_x, (arrow_y-(y_diff/arrow_length_fraction))), 
                      arrowprops=dict(arrowstyle='simple', fc='k'), 
                      clip_on=True
                     )
 
     self.ax.text(x=arrow_x, 
-                 y=(arrow_y-(y_diff/arrow_text_fraction[self.fig_size])), 
+                 y=(arrow_y-(y_diff/arrow_text_fraction)), 
                  s='N', 
                  horizontalalignment='center'
                 )
     
     #Plot scale bar
-    sb_coordinates = scale_bar_coordinates(self.x_lim_island()[0], 
-                                           self.y_lim_island()[0], 
-                                           self.x_lim_island()[1],
-                                           self.y_lim_island()[1]
-                                          )
+    sb_coordinates = scale_bar_coordinates()
     
     scale_L_coord = sb_coordinates['scale_L_coord']
     scale_R_coord = sb_coordinates['scale_R_coord']
@@ -228,24 +205,34 @@ class GalapagosMap:
     white1_x_coords = sb_coordinates['white1_x_coords']
     white2_x_coords = sb_coordinates['white2_x_coords']
     text_y_coordinate = sb_coordinates['text_y_coordinate']
-    middle_text_x_coord = sb_coordinates['middle_text_x_coord']
-    middle_text_label = sb_coordinates['middle_text_label']
     closest_scale_bar_label = sb_coordinates['closest_scale_bar_label']
         
     self.ax.plot((scale_L_coord, scale_R_coord), (scale_y_coord, scale_y_coord), color='k', linestyle='-', linewidth=4),
     self.ax.plot(white1_x_coords, (scale_y_coord, scale_y_coord), color='w', linestyle='-', linewidth=2.5),
     self.ax.plot(white2_x_coords, (scale_y_coord, scale_y_coord), color='w', linestyle='-', linewidth=2.5),
     self.ax.text(scale_L_coord, text_y_coordinate, '0', ha='center'),
-    self.ax.text(middle_text_x_coord, text_y_coordinate, str(middle_text_label), ha='left'),
     self.ax.text(scale_R_coord, text_y_coordinate, str(closest_scale_bar_label) + ' km', ha='left')
-    
+
+    #Trying to place using pixel coordinates
+    bbox = self.ax.get_window_extent().transformed(self.fig.dpi_scale_trans.inverted())
+    width, height = bbox.width, bbox.height
+
+    width *= self.fig.dpi
+    height *= self.fig.dpi
+
     #Zoom into desired island
     self.ax.set_xlim(self.x_lim_island())
     self.ax.set_ylim(self.y_lim_island())
     
-    #Remove axis labels
-    self.ax.axes.get_xaxis().set_ticklabels([])
-    self.ax.axes.get_yaxis().set_ticklabels([])
+    #Remove axis labels and ticks
+    #self.ax.axes.get_xaxis().set_ticklabels([])
+    #self.ax.axes.get_yaxis().set_ticklabels([])
+    
+    #self.ax.axis('on')
+    self.ax.xaxis.set_major_locator(plt.NullLocator())
+    self.ax.yaxis.set_major_locator(plt.NullLocator())
+
+
   #Accepts a rasterio raster and matplotlib **kwargs for plt.imshow()
   def add_raster_data(self, raster_data, raster_band=1, **kwargs):
     plt.imshow(raster_data.read(raster_band), 
@@ -258,7 +245,7 @@ class GalapagosMap:
       
   #Accepts geospatial dataframe data
   def add_vector_data(self, vector_data, **kwargs):
-      vector_data.geometry.plot(ax=self.ax, **kwargs)
+    vector_data.geometry.plot(ax=self.ax, **kwargs)
   
   #Add an ocean color from a matplotlib compatible color string
   def add_ocean_color(self, color):
@@ -266,119 +253,10 @@ class GalapagosMap:
   
   #Add attribution text to the bottom left corner of the plot
   def add_attribution_text(self, attribution_text, **kwargs):
-
-    text_buffer_fraction = {'large' : 50,
-                            'medium' : 40,
-                            'small' : 30,
-                           }
     
-    x_diff = self.x_lim_island()[1] - self.x_lim_island()[0]
-    y_diff = self.y_lim_island()[1] - self.y_lim_island()[0]
-    
-    text_x_buffer = x_diff / text_buffer_fraction[self.fig_size]
-    text_y_buffer = y_diff / text_buffer_fraction[self.fig_size]
-    
-    self.ax.text(x=self.x_lim_island()[0] + text_x_buffer,
-        y=self.y_lim_island()[0] + text_y_buffer,
+    self.ax.text(x=self.x_lim_island()[0] + self.arrow_x_buffer,
+        y=self.y_lim_island()[0] + self.arrow_y_buffer,
         s=attribution_text, **kwargs)
   
   def add_grid(self, **kwargs):
     self.ax.grid(True, **kwargs)
-  
-'''
-Internal class to define bounding boxes around each island
-'''
-class Bounding_Boxes:
-  from shapely.geometry import mapping
-  global mapping
-
-  import csv
-  global csv
-'''  
-  #Instantiate Bounding_Boxes class using vector data - this class is written specifically to accept volcano specific data
-#  def __init__(self, vector_data):
-  def __init__(self):
-
-    #Add a relative path pointing to the correct csv file
-    #Create a dict from each record, this will be self.bboxes
-    bbox = {}
-    
-    geometry_csv = open('./bounding_box_geometry.csv', 'rb')
-    
-    csv_reader = csv.reader(geometry_csv, delimiter=',')
-    for row in csv_reader:
-      bbox[row[0]] = row[1]
-
-    geometry_csv.close()
-     
-    #Add vector_data attribute to inspect original vector data
-    self.vector_data = vector_data
-
-    bbox = {}
-    bbox['Archipelago'] = vector_data.total_bounds
-
-    isabela_list = ['Darwin', 'Alcedo', 'Ecuador', 'South', 'Wolf']
-
-    bbox['Isabela'] = vector_data[vector_data['Name'].isin(isabela_list)].total_bounds
-
-    for island in list(vector_data.Name.unique()):
-      bbox[island] = vector_data[vector_data['Name']==island].envelope
-    
-    
-    self.bboxes = bbox
-
-    #Add volcano_names class to inspect all unique volcano names (useful for plotting)
-    volcano_names = list(vector_data.Name.unique()) + ['Archipelago'] + ['Isabela']
-    
-    self.volcano_names = volcano_names
-    
-    self.volcano_names = bbox.keys()
-  #Define a method to create matplotlib compatible xlim
-  def x_lim_island(self, island, zoom_factor=0.15):
-
-    #Define the factor by which to zoom out x axis (this could be made into two zooms L & R)
-    #z_factor = 0.15 means that 15% of the total boundign box will be added to both sides
-    #This scales with the size of the island to add a border to all plots
-    z_factor = zoom_factor
-
-    #Archipelago is a ndarray data type not a Shapely Polygon
-    if island in ['Archipelago', 'Isabela']:
-      xmin = self.bboxes[island][0]
-      xmax = self.bboxes[island][2]
-
-      zoom = (xmax - xmin) * z_factor
-
-      #Returns xlim ready for the plt.xlim function
-      return(xmin - zoom, xmax + zoom)
-
-    #Extract coordinates from Shapely Polygon bounding boxes
-    else:
-      xmin = mapping(self.bboxes[island])['bbox'][0]
-      xmax = mapping(self.bboxes[island])['bbox'][2]
-
-      zoom = (xmax - xmin) * z_factor
-
-      #Returns xlim ready for the plt.xlim function    
-      return(xmin - zoom, xmax + zoom)
-
-  #Copy of x_lim_island for the y axis
-  def y_lim_island(self, island, zoom_factor=0.15):
-
-    z_factor = zoom_factor
-
-    if island in ['Archipelago', 'Isabela']:
-      ymin = self.bboxes[island][1]
-      ymax = self.bboxes[island][3]
-
-      zoom = (ymax - ymin) * z_factor
-
-      return(ymin - zoom, ymax + zoom)
-
-    else:
-      ymin = mapping(self.bboxes[island])['bbox'][1]
-      ymax = mapping(self.bboxes[island])['bbox'][3]
-
-      zoom = (ymax - ymin) * z_factor
-
-      return(ymin - zoom, ymax + zoom) 
-'''
