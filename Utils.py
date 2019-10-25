d7bcf7b7527edaa7c523de81ed8e339cdec095cb
hamishgibbs <hamishgibbs@Hamishs-MacBook-Air.local>
2019-10-25 11:53:14 +0100

    Utils.py

--- Utils.py
+++ Utils.py
@@ -7,14 +7,14 @@ Created on Thu Oct 17 11:58:48 2019
 """
 
 #For use mapping relatively flat islands (changing the cutoff of a color map)
-  def truncate_colormap(self, cmap, minval=0.0, maxval=1.0, n=100):
-    from matplotlib.colors import LinearSegmentedColormap
-    global LinearSegmentedColormap
+def truncate_colormap(self, cmap, minval=0.0, maxval=1.0, n=100):
+  from matplotlib.colors import LinearSegmentedColormap
+  global LinearSegmentedColormap
 
-    import numpy as np
-    global np
+  import numpy as np
+  global np
     
-    new_cmap = LinearSegmentedColormap.from_list(
-        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
-        cmap(np.linspace(minval, maxval, n)))
-    return new_cmap
+  new_cmap = LinearSegmentedColormap.from_list(
+      'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+      cmap(np.linspace(minval, maxval, n)))
+  return new_cmap
